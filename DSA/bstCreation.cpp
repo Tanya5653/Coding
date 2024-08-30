c7d6c7a2dbd2f934bf39e7d006b4a41f8943befe
Tanya5653 <tanyakumari5653@gmail.com>
2024-08-30 16:08:37 +0530

    bst added

---
+++ DSA/bstCreation.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+    //intialized a constructor
+    Node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* insertIntoBST(Node* root,int d){
+    //base case
+    if(root==NULL){
+        root=new Node(d);
+        return root;
+    }
+    if(d < root->data){
+        root->left = insertIntoBST(root->left,d);
+    }
+    else{
+        root->right = insertIntoBST(root->right,d);
+    }
+    return root;
+}
+void takeInput(Node* &root){
+    int data;
+    cin>>data;
+    while(data!=-1){
+        root = insertIntoBST(root,data);
+        cin>>data;
+    }
+}
+int main(){
+    Node* root = NULL;
+    cout<<"Enter data for creating bst:"<<endl;
+    takeInput(root);
+    cout<<"Printing BST:"<<endl;
+    return 0;
+}
