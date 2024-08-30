d5ff19eaaf60c8c3f6f70c2f0f9b19bc3a1c5cd0
Tanya5653 <tanyakumari5653@gmail.com>
2024-08-30 17:51:47 +0530

    bst edited

--- DSA/bstCreation.cpp
+++ DSA/bstCreation.cpp
@@ -40,6 +40,44 @@ Node* maxVal(Node* root){
     }
     return temp;
 }
+Node* deleteFromBST(Node* root,int x){
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->data==x){
+        //0 child
+        if(root->left==NULL && root->right==NULL){
+            delete root;
+            return NULL;
+        }
+        //1child
+        if(root->left!=NULL && root->right==NULL){
+            Node* temp= root->left;
+            delete root;
+            return temp;
+        }
+        if(root->left==NULL && root->right !=NULL){
+            Node* temp= root->right;
+            delete root;
+            return temp;
+        }
+        //2child
+        if(root->left!=NULL && root->right!=NULL){
+            int mini = minVal(root->right)->data;
+            root->data = mini;
+            root->right = deleteFromBST(root->left,mini);
+            return root;
+        }
+    }
+    else if(root->data < x){
+        root->right = deleteFromBST(root->right,x);
+        return root;
+    }
+    else{
+        root->left = deleteFromBST(root->left,x);
+        return root;
+    }
+}
 void takeInput(Node* &root){
     int data;
     cin>>data;
